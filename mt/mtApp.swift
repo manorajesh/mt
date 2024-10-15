56ff2612b50cf954e60d848bc83568c5a2e93f85
manorajesh <mano@raje.sh>
2024-10-14 22:31:22 -0700

    Initial Commit

---
+++ mt/mtApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtApp.swift
+//  mt
+//
+//  Created by Mano Rajesh on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct mtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
